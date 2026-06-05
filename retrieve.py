8ccff51a009afca56d33f613045469316d4589bc
Dhruvish-28 <dhruvishparikh28@gmail.com>
2026-06-05 15:18:47 +0530

    Updated retrieval process with Ingestion pipeline

--- retrieve.py
+++ retrieve.py
@@ -1,31 +1,25 @@
 # from embedding_model import model
-from create_embeddings import generate_embeddings
+from ingestion import generate_embeddings
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-import json 
 
-chunk_embedding = np.load("vector_db/embeddings.npy")
-with open("vector_db/chunks.json", "r", encoding="utf-8") as f:
-    chunks = json.load(f)
-
-def retrieval(query , top_k = 3):
-
+def retrieval(query , chunks , chunk_embedding,top_k = 3):
     
     query_embedding = generate_embeddings(query)
 
     similarities = cosine_similarity( [query_embedding], chunk_embedding)
-
-    similarities.sort()
-
-
+    
     indices = np.argsort(similarities[0])[-top_k:][::-1]
 
     retrieved_chunks = []
-
+        
     for idx in indices:
+
+        score = float(similarities[0][idx])
+        score = round(float(similarities[0][idx]), 4)
         retrieved_chunks.append({
             "index": idx,
-            "score": similarities[0][idx],
+            "score": score,
             "chunk": chunks[idx]
         })
 
