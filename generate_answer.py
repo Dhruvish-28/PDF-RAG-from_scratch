6bce9c05254dabd5b5171cf2420daf6f7b61aaa3
Dhruvish-28 <dhruvishparikh28@gmail.com>
2026-06-05 18:34:14 +0530

    Implemented answer generation through model

---
+++ generate_answer.py
@@ -0,0 +1,25 @@
+from retrieve import retrieval
+from prompt_builder import build_prompt
+from gemini_model import model
+
+def generate_answer(query, chunks , chunk_embedding):
+    """
+    parameters:
+    query : user passed query
+    chunks : chunks from pdf
+    chunk_embeddings : embeddings of all chunks
+
+    passes all 3 things to retrieve.py to get top 3 relevant chunks and scores.
+    Then control goes to prompt_builder to obtain prompt to be given to LLM
+    At last LLM generates response
+    """
+    
+    selected_chunks = retrieval(query, chunks , chunk_embedding)
+
+    prompt,score = build_prompt(selected_chunks, query)
+
+    print(f"Similarity Scores of top 3 chunks: {score}")
+    
+    response = model.generate_content(prompt)
+
+    return response.text
