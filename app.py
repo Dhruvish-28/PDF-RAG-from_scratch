7a84ccb812195761fc9c9ec8eb9c0e1bad5071c7
Dhruvish-28 <dhruvishparikh28@gmail.com>
2026-06-07 12:46:06 +0530

    Implemented chat history feature

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ st.set_page_config(
     page_icon="📄",
     layout="wide"
 )
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
 st.header("**RAG IMPLEMENTED STUDY ASSISTANT**")
 st.divider()
 
@@ -57,9 +61,11 @@ if file is not None:
     st.session_state.chunks = chunks
     st.session_state.chunk_embedding = chunk_embedding
 
-
-
-    
+for message in st.session_state.messages:
+                
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+        
 question = st.chat_input("Enter your Question:")
     
 if question:
@@ -81,7 +87,11 @@ if question:
 
                 with st.chat_message("assistant"):
                     st.markdown(answer)
-                    
+
+                st.session_state.messages.append({ "role": "user", "content": question })
+
+                st.session_state.messages.append({"role": "assistant", "content": answer})
+                        
                 with st.sidebar:
                     
                     with st.expander("Retrieved Chunks"):
